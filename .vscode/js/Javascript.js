4bbc718a9828fd2fbccd5b4c392c2496de19e96e
pediazr <pedro.neox@gmail.com>
2023-08-10 12:20:53 -0400

    Primer Commit

---
+++ .vscode/js/Javascript.js
@@ -0,0 +1,2 @@
+var dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"] ; //array dias de la semana
+for(var i=0; i<dias.length; i++){alert(dias[i])}; //muestra arreglo
